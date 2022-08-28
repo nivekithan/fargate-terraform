4328073835d599cababf351b323685fbc9467371
Nivekithan S <nivekithan363@gmail.com>
2022-08-28 15:01:53 +0530

    Init terraform code

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+ required_providers {
+   aws = {
+    source = "hashicorp/aws"
+    version = "~> 4.0"
+   }
+ }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+  profile = "terraform"
+}
+
+
+resource "aws_vpc" "fargte_main" {
+  cidr_block = "10.0.0.0/16"
+}
